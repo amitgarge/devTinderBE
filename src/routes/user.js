87714fc6d3728c7928df8715b8699c82b0146271
Amit <amit.garge19@gmail.com>
2026-02-05 17:46:48 +0530

    feed API with pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -52,4 +52,39 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(400).send({ message: error.message });
   }
 });
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequestsOfLoggedInUser = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideFromFeed = new Set();
+    connectionRequestsOfLoggedInUser.forEach((request) => {
+      hideFromFeed.add(request.fromUserId);
+      hideFromFeed.add(request.toUserId);
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json(users);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 module.exports = userRouter;
